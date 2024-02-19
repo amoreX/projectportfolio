6fc0adc7ded29b93dc91b5d8acc119909dba5ead
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-02-20 02:06:41 +0530

    working-on-doch

--- src/Comp/Custom.jsx
+++ src/Comp/Custom.jsx
@@ -3,7 +3,9 @@ import {motion} from 'framer-motion'
 
 export default function Custom(){
     return(
-        <motion.div>
+        <motion.div
+        id="custom"
+        >
             ok
         </motion.div>
     )
--- src/Comp/Doch/Doch.jsx
+++ src/Comp/Doch/Doch.jsx
@@ -1,8 +1,30 @@
 
 import {motion} from "framer-motion"
 import Custom from "../Custom"
+import { useEffect } from "react"
 import "./style.css"
 export default function Doch(){
+
+	useEffect(()=>{
+		const handleWheel = (event) => {
+			if (event.deltaY !== 0) {
+			  event.preventDefault();
+			  window.scrollBy({
+				left: event.deltaY * 5,
+				behavior:"smooth",
+			  });
+			}
+		};
+
+		document.addEventListener("wheel", handleWheel);
+
+    return () => {
+      // Cleanup: Remove the event listener when the component unmounts
+      document.removeEventListener("wheel", handleWheel);
+    };
+
+
+	},[]);
 	return(	
 		<motion.div
 		id="main-container-doch"
