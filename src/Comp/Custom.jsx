f79523c63c8900e18bf5b745eddcb3e0751dd210
WahidX <wahidx93@gmail.com>
2024-02-23 00:17:21 +0530

    almost-done

--- src/Comp/Custom.jsx
+++ src/Comp/Custom.jsx
@@ -1,5 +1,4 @@
-import { useEffect,useRef } from 'react'
-import {motion,useInView,useAnimation} from 'framer-motion'
+import {motion} from 'framer-motion'
 
 export default function Custom(){
 
@@ -11,6 +10,30 @@ export default function Custom(){
         >
             <div id="page-1">
                 <div id="pictures">
+                    <motion.div
+                    variants={{
+                        hidden:{top:"-60%"},
+                        visible:{top:"5%"}
+                    }}
+                    transition={{type:"tween",duration:0.56,delay:0.1,ease:"easeInOut"}}
+                    initial="hidden"
+                    whileInView="visible"
+                    id="pic-1"
+                    >
+                        <img  src="/Assets/doch1.png" alt="doch" ></img>
+                    </motion.div>
+                    <motion.div
+                    variants={{
+                        hidden:{top:"100%"},
+                        visible:{top:"45%"}
+                    }}
+                    transition={{type:"tween",duration:0.56,delay:0.1,ease:"easeInOut"}}
+                    initial="hidden"
+                    whileInView="visible"
+                    id="pic-2"
+                    >
+                        <img  src="/Assets/doch2.png" alt="doch" ></img>
+                    </motion.div>
 
                 </div>
                 <div id="text-div">
@@ -64,7 +87,27 @@ export default function Custom(){
                 </div>
             </div>
             <div id="page-2">
-                page2
+
+                <motion.div
+                id="video"
+                variants={{
+                    hidden:{top:"-70%",opacity:0},
+                    visible:{top:"10%",opacity:1},
+                }}
+                transition={{type:"tween",duration:0.56,delay:0.1,ease:"easeInOut"}}
+                initial="hidden"
+                whileInView="visible"
+                >
+                     <video   autoPlay loop muted>
+                        <source src="/Assets/dochvid.mp4" type="video/mp4" />
+                    </video>
+                </motion.div>
+
+                <div id="page2-text">
+
+                    
+
+                </div>
             </div>
         </motion.div>
     )
