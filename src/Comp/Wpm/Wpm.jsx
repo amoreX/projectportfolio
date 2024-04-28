5b1fcc759e22c3c0ed25ea21e809a4d0e43a2563
N!hal <71655709+amoreX@users.noreply.github.com>
2024-04-28 23:11:11 +0530

    Update Wpm.jsx

--- src/Comp/Wpm/Wpm.jsx
+++ src/Comp/Wpm/Wpm.jsx
@@ -67,7 +67,7 @@ export default function Wpm(){
 				id="text-cover"
 				initial={{}}
 				transition={{type:"tween",delay:0.2,duration:0.38,ease:"easeInOut"}}
-				animate={{x:1500}}></motion.div>
+				animate={{x:3500}}></motion.div>
 
 				</div>
 				<motion.div
